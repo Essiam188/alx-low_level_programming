5bd28436371395f9b61f7fb7e0720f5508bd6010
Essiam188 <essiamp188@gail.com>
2023-07-14 22:05:56 -0700

    101-quote.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#inlcude <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammmr, but the outcome is a piece of art");
+	return (0);
+}
